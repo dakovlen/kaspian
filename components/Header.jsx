559b174e01b919db60b33b4423b3a6e03624a45c
Artem Koval <dakovlen@gmail.com>
2022-04-21 14:21:22 +0300

    Add component Layout, fix file for layout

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,18 +9,18 @@ const categories = [
 
 const Header = () => {
   return (
-    <header className='container mx-auto mb-8'>
-      <div className='border-b w-full flex items-center justify-between border-blue-400 py-5'>
-        <div className='md:flex'>
+    <header className='container mx-auto px-10 mb-8'>
+      <div className='border-b w-full inline-block border-blue-400 py-8'>
+        <div className='md:float-left block'>
           <Link href="./">
             <span className='cursor-pointer font-bold text-4xl text-white'>Logo</span>
           </Link>
         </div>
 
-        <div className=''>
+        <div className='hidden md:float-right md:contents'>
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className='mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+              <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
                 {category.name}
               </span>
             </Link>
--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => <PostCard post={post} key={post.title} />)}
