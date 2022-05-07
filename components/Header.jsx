0f9dc499d5d9e5038dbe662d178578d6bf82bcc7
Artem Koval <dakovlen@gmail.com>
2022-05-07 16:26:03 +0300

    Update: remove

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     <header className='container mx-auto px-10 mb-8'>
       <div className='border-b w-full inline-block border-blue-400 py-5'>
         <div className='md:float-left block'>
-          <Link href="./" passHref>
+          <Link href="#" passHref>
             <span className='cursor-pointer font-bold text-4xl text-white'>Logo</span>
           </Link>
         </div>
