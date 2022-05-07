a7ac1598268dc3a6451edbfd8e9f1a5d422ec112
Artem Koval <dakovlen@gmail.com>
2022-05-07 12:46:01 +0300

    Add get post, add img to project

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ const categories = [
 const Header = () => {
   return (
     <header className='container mx-auto px-10 mb-8'>
-      <div className='border-b w-full inline-block border-blue-400 py-8'>
+      <div className='border-b w-full inline-block border-blue-400 py-5'>
         <div className='md:float-left block'>
           <Link href="./">
             <span className='cursor-pointer font-bold text-4xl text-white'>Logo</span>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
-import Head from 'next/head'
-import { Categories, PostCard, PostWidget, Header } from '../components'
+import Head from 'next/head';
+import { Categories, PostCard, PostWidget, Header } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  {
-    title: "1Test title",
-    exert: "1React developer text"
-  },
-  {
-    title: "2Test title",
-    exert: "2React developer text"
-  },
-]
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -22,7 +13,10 @@ export default function Home() {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+          
         </div>
         <div className="lg:col-span-4 col-span1">
           <div className="lg:sticky relative top-8">
@@ -34,4 +28,12 @@ export default function Home() {
       
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
