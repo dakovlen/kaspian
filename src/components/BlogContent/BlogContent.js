c9ff0d3bb22c7bad611ed3a9934fb1bff29317d1
Artem Koval <dakovlen@gmail.com>
2022-06-02 00:22:47 +0300

    Update: add arr, add shared folder in project

--- src/components/BlogContent/BlogContent.js
+++ src/components/BlogContent/BlogContent.js
@@ -1,42 +1,29 @@
 import React from "react";
-import './blog-content.css';
+import { posts } from "../../shared/projectData";
+import { getAmountPosts } from "../../shared/projectLogic";
+import "./blog-content.css";
+
+const blogPosts = posts.map((item) => {
+  return (
+    <div key={item.id} className="post">
+      <h2>{item.title}</h2>
+      <p>
+        {item.description}
+      </p>
+    </div>
+  );
+});
 
 export const BlogContent = () => {
   return (
     <>
       <h1>Simple Blog</h1>
       <div className="posts">
-        <div className="post">
-          <h2>Post 1</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
-            fugiat harum. Voluptatibus beatae corrupti nulla, qui odit mollitia
-            doloremque rerum magni rem aut laborum, maiores officiis laboriosam
-            hic. Ratione, voluptas?
-          </p>
-        </div>
-        <div className="post">
-          <h2>Post 2</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
-            fugiat harum. Voluptatibus beatae corrupti nulla, qui odit mollitia
-            doloremque rerum magni rem aut laborum, maiores officiis laboriosam
-            hic. Ratione, voluptas?
-          </p>
-        </div>
-        <div className="post">
-          <h2>Post 3</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
-            fugiat harum. Voluptatibus beatae corrupti nulla, qui odit mollitia
-            doloremque rerum magni rem aut laborum, maiores officiis laboriosam
-            hic. Ratione, voluptas?
-          </p>
-        </div>
+        {blogPosts}
       </div>
 
       <div className="count">
-        <button>Get amount of posts</button>
+        <button onClick={() => {getAmountPosts(posts)}}>Get amount of posts</button>
       </div>
     </>
   );
---
+++ src/shared/projectLogic.js
@@ -0,0 +1,3 @@
+export const getAmountPosts = (arr) => {
+  console.log(`Amount of posts ${arr.length}`)
+}
