5cda2bb08ca90c10fe0d9c952c6f569f477edfc2
Artem Koval <dakovlen@gmail.com>
2022-06-08 09:36:01 +0300

    Update: change component to classes component

--- src/components/BlogContent/BlogContent.js
+++ src/components/BlogContent/BlogContent.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import { posts } from "../../shared/projectData";
 import "./BlogContent.css";
 import { BlogCard } from "./components/BlogCard";
 
-const blogPosts = posts.map((item) => {
-  return (
-    <BlogCard 
-      key={item.id}
-      title={item.title}
-      description={item.description}
-    />
-  );
-});
+export class BlogContent extends Component {
+  
+  blogPosts = posts.map((item) => {
+    return (
+      <BlogCard 
+        key={item.id}
+        title={item.title}
+        description={item.description}
+      />
+    );
+  });
 
-export const BlogContent = () => {
-  return (
-    <>
-      <h1>Simple Blog</h1>
-      <div className="posts">
-        {blogPosts}
-      </div>
-    </>
-  );
+
+  render() {
+    return (
+      <>
+        <h1>Simple Blog</h1>
+        <div className="posts">
+          {this.blogPosts}
+        </div>
+      </>
+    );
+  }
 };
